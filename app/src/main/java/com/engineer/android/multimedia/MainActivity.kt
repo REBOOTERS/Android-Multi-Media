38d45aa2a380545bf4547ce59b3655bb4abca8af
rookie <yingkongshi11@gmail.com>
2019-09-05 13:28:26 +0800

    add media module

--- app/src/main/java/com/engineer/android/multimedia/MainActivity.kt
+++ app/src/main/java/com/engineer/android/multimedia/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.engineer.android.multimedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.engineer.android.media.ui.SimpleMediaPlayerActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        media.setOnClickListener { startActivity(Intent(this,SimpleMediaPlayerActivity::class.java)) }
     }
 }
---
+++ media/src/main/java/com/engineer/android/media/base/Constant.kt
@@ -0,0 +1,13 @@
+package com.engineer.android.media.base
+
+import com.engineer.android.media.R
+
+/**
+ * @author zhuyongging @ Zhihu Inc.
+ * @since 09-05-2019
+ */
+class Constant {
+    companion object {
+        val MEDIA_RES_ID = R.raw.jazz_in_paris
+    }
+}
