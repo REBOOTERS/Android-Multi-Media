17c2cbcb9a9948bd18a760dec9cbba5c6005e2e9
rookie <yingkongshi11@gmail.com>
2021-02-13 11:44:29 +0800

    fix some issue

--- media/src/main/cpp/native-lib.cpp
+++ media/src/main/cpp/native-lib.cpp
@@ -5,18 +5,49 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
+
+extern "C" {
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+
+JNIEXPORT jstring JNICALL
 Java_com_engineer_android_media_ui_FFmpegActivity_getFFmpegInfo(
         JNIEnv *env,
         jobject /*this*/
 ) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char info[40000] = {0};
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%sdecode:", info);
+        } else {
+            sprintf(info, "%sencode:", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s(video):", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s(audio):", info);
+                break;
+            default:
+                sprintf(info, "%s(other):", info);
+                break;
+        }
+        sprintf(info, "%s[%s]\n", info, c_temp->name);
+        c_temp = c_temp->next;
+    }
+
+    return env->NewStringUTF(info);
 }
 
-extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_engineer_android_media_ui_FFmpegActivity_getHello(JNIEnv *env, jobject thiz) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
 }
